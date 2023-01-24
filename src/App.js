6bdb572601b8da1e89990aa615c3a9ae7691c463
Omowunmi Adesuyi <adesuyiomowunmi@gmail.com>
2023-01-24 12:06:15 +0100

    logic of expenseitem

--- src/App.js
+++ src/App.js
@@ -1,9 +1,55 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 200.72,
+      date: new Date("2020, 01, 24"),
+    },
+    {
+      id: "e2",
+      title: "Health Insurance",
+      amount: 234.2,
+      date: new Date("2021, 08, 14"),
+    },
+    {
+      id: "e3",
+      title: "Food",
+      amount: 546.12,
+      date: new Date("2022, 10, 13"),
+    },
+    {
+      id: "e4",
+      title: "Utility",
+      amount: 892.52,
+      date: new Date("2023, 01, 28"),
+    },
+  ];
+
   return (
     <div>
-      <ExpenseItem />
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+      ></ExpenseItem>
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,13 @@
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
+function ExpenseItem(props) {
   return (
     <div className="expense-item">
-      <div>January 5th 2021</div>
+      <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>Let's Go!</h2>
+        <h2>{props.title}</h2>
       </div>
-      <div className="expense-item__price">$200</div>
+      <div className="expense-item__price">${props.amount}</div>
     </div>
   );
 }
