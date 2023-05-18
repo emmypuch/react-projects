9e84ac2294fa110295a1c35b66f01ef4762f59a7
Omowunmi Adesuyi <adesuyiomowunmi@gmail.com>
2023-05-18 01:03:45 +0100

    wip

---
+++ react-calculator/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="calculator-grid">
+      <div className="output">
+        <div className="previous-operand"></div>
+        <div className="current-operand"></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
