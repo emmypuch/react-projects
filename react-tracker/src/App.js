d91534f9fafb934ac42497c8f900043d34aefbf2
Omowunmi Adesuyi <adesuyiomowunmi@gmail.com>
2023-05-19 22:17:45 +0100

    added incomeexpense and other components

--- react-tracker/src/App.js
+++ react-tracker/src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
 import { Header } from "./components/Header";
+import { Balance } from "./components/Balance";
+import { IncomeExpense } from "./components/IncomeExpense";
+import { TransactionList } from "./components/TransactionList";
 import "./App.css";
 
 function App() {
   return (
     <div>
       <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpense />
+      </div>
+      <TransactionList />
     </div>
   );
 }
