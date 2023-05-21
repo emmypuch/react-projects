2dbc8ed1322d0035abe427e878335d715a0a3be9
Omowunmi Adesuyi <adesuyiomowunmi@gmail.com>
2023-05-22 00:34:53 +0100

    fixing tailwindcss bug

--- react-gallery/src/App.js
+++ react-gallery/src/App.js
@@ -3,9 +3,15 @@ import React from "react";
 function App() {
   return (
     <div className="max-w-sm rounded overflow-hidden shadow-lg">
-      <img src="https://source.unsplash.com/random" alt="" className="w-full" />
+      <img
+        src="https://source.unsplash.com/random"
+        alt=""
+        className="w-full h-8"
+      />
       <div className="px-6 py-4">
-        <h1 className="text-red-600">Hiii</h1>
+        <div className="font-bold text-purple-500 text-xl mb-2">
+          Photo by John Doe
+        </div>
       </div>
     </div>
   );
